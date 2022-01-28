f13c6fa271538e07b7f44585cf71d66cf062b4d9
DongHeon Lee <niche3600@gmail.com>
2022-01-28 16:24:15 +0900

    add hot module reload

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -1,27 +1,66 @@
 'use strict';
 
-import path from 'path';
 import { Configuration } from 'webpack';
 
 import 'webpack-dev-server';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-import base from './webpack.common';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const isDev = process.env.NODE_ENV === 'development';
 
 const front: Configuration = {
-  ...base,
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: isDev },
+          },
+        ],
+      },
+      {
+        test: /\.(bmp|ico|gif|jpe?g|png|svg|ttf|eot|woff?2?)$/,
+        // ファイルそのまま取り込む
+        // type: 'asset/resource',
+        // Base64にして取り込む
+        type: 'asset/inline',
+      },
+    ],
+  },
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
+  output: {
+    filename: "bundle.js"
+  },
   devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-      publicPath: '/'
-    },
     historyApiFallback: true,
     compress: true,
     hot: true,
     open: true,
     port: 4000,
   },
+  plugins: [new HtmlWebpackPlugin({
+    template: "src/index.html",
+    hash: true, // This is useful for cache busting
+    filename: 'index.html'
+  })]
 };
 
 export default front;
